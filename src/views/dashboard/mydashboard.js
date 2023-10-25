7e3fdc82138b435e4dc235b0f43a0d1b3089eb05
Mukesh-Thidiff <mukesh.murugaiyan@thidiff.com>
2023-10-25 09:34:37 +0530

    update pr link for drawer

--- src/views/dashboard/mydashboard.js
+++ src/views/dashboard/mydashboard.js
@@ -1,42 +1,44 @@
-import { DrawerContentScrollView, DrawerItemList, createDrawerNavigator } from '@react-navigation/drawer';
+import { FontAwesome } from "@expo/vector-icons";
+import {
+  DrawerContentScrollView,
+  createDrawerNavigator
+} from "@react-navigation/drawer";
 import axios from "axios";
-import React, { useEffect, useState } from 'react';
-import { Image, StatusBar, Text, View } from 'react-native';
-import LottieView from 'lottie-react-native';
-import { Menu, MenuOption, MenuOptions, MenuTrigger } from 'react-native-popup-menu';
+import LottieView from "lottie-react-native";
+import React, { useEffect, useState } from "react";
+import { Image, StatusBar, Text, TouchableOpacity, View } from "react-native";
 import DotMenu from "../../components/Menu";
-
 const Drawer = createDrawerNavigator();
 
 const DashboardScreen = () => {
   const [weatherData, setWeatherData] = useState(null);
 
   useEffect(() => {
-    // Replace 'YOUR_API_KEY' with your actual API key
-    const apiKey = 'b349593e7cf077168b90cdf6a796f787';
-    const city = 'Mayiladuthurai';
+    const apiKey = "b349593e7cf077168b90cdf6a796f787";
+    const city = "Mayiladuthurai";
 
     axios
-    .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
-    .then((response) => {
-      console.log('Temperature in Celsius:', response.data.main.temp);
-      setWeatherData(response.data);
-    })
-    .catch((error) => {
-      console.error('Error fetching weather data:', error);
-    });
-  
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+      )
+      .then((response) => {
+        console.log("Temperature in Celsius:", response.data.main.temp);
+        setWeatherData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
   }, []);
-  let animationFile = 'animation_lnn10e95.json'; // Replace with the correct animation file
+  let animationFile = "animation_lnn10e95.json"; // Replace with the correct animation file
 
   // if (weatherData && weatherData.main.temp <= 10) {
-  //   animationFile = 'snowy-animation.json';
+  //   animationFile = "snowy-animation.json";
   // } else if (weatherData && weatherData.main.temp > 10 && weatherData.main.temp <= 25) {
-  //   animationFile = 'sunny-animation.json';
+  //   animationFile = "sunny-animation.json";
   // } else if (weatherData && weatherData.main.temp > 25) {
-  //   animationFile = 'rainy-animation.json';
+  //   animationFile = "rainy-animation.json";
   // }
-console.log('animationFile >>>----------------------------->',animationFile);
+  console.log("animationFile >>>----------------------------->", animationFile);
   return (
     <View>
       <StatusBar backgroundColor="gray" />
@@ -58,31 +60,52 @@ console.log('animationFile >>>----------------------------->',animationFile);
 };
 
 
-
 const CustomDrawerContent = (props) => {
+  const menuItems = [
+    { name: "Dashboard", icon: "home" }, 
+    { name: "Setting", icon: "setting" }, 
+  ];
+
+  const renderCustomItem = (item, index) => (
+    <TouchableOpacity
+      key={index}
+      onPress={() => props.navigation.navigate(item.name)}
+      style={{ flexDirection: "row", alignItems: "center", padding: 20 }}
+    >
+        <FontAwesome name={item.icon} size={25} color="black" />
+      <View style={{ marginLeft: 10 }}>
+      <Text>{item.name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
     <DrawerContentScrollView {...props}>
-    <View style={{ flexDirection: 'row', alignItems: 'center', padding: 8, backgroundColor: 'skyblue', justifyContent:"space-between" }}>
-      <View style={{flexDirection: 'row', alignItems: 'center'}}>
-      <Image
-        source={require('../../../assets/sm.jpg')}
-        style={{ width: 50, height: 50, borderRadius: 25 }}
-      />
-      <View style={{ marginLeft: 16 }}>
-        <Text>Mukesh M...</Text>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          padding: 10,
+          justifyContent: "center",
+          borderBottomWidth: 1,
+          borderBottomColor: "gray",
+          marginBottom: 2,
+        }}
+      >
+        <View style={{ alignItems: "center", width: 100 }}>
+          <Image
+            source={require("../../../assets/sm.jpg")}
+            style={{ width: 60, height: 60, borderRadius: 30 }}
+          />
+          <View style={{ marginLeft: 16, marginTop: 5, marginBottom: 5 }}>
+            <Text style={{ fontWeight: "bold" }}>Mukesh M...</Text>
+          </View>
+        </View>
       </View>
-      </View>
-  <View>
-     <DotMenu/>
-     </View>
-     
-    </View>
-    <DrawerItemList {...props} />
-  </DrawerContentScrollView>
-  
+      {menuItems.map((item, index) => renderCustomItem(item, index))}
+    </DrawerContentScrollView>
   );
 };
-
 
 const mydashboard = ({ navigation }) => {
   return (
